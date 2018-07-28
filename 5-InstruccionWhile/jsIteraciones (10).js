4a85ad807847e5a2884c42e246d0b46fc96609b6
yanettolaba <yane_tolaba@hotmail.com>
2018-07-28 10:40:54 -0300

    ejercicio 10 explicado en clase
    completar

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,17 +1,24 @@
 function mostrar()
 {
-
-	var contador=0;
 	//declarar contadores y variables 
 	
-	var respuesta="si";
+	var respuesta = true;
 	var numero;
-	negativo = 0;
+	var acumuladorNegativo = 0;
+	var acumuladorPositivos = 0; 
+	var contadorPositivos = 0;
+	var contadorNegativos = 0;
+	var contadorCeros = 0;
+	var promedioPositivos;
+	var promedioNegativos;
+	var diferencia;
+	var maximo;
+	var flag = false;
 
-	while(respuesta!="no")
-	
-	{	numero = prompt("ingrese numero");
+	while(respuesta == true ){
+		numero = prompt("ingrese numero");
 		numero = parseInt(numero);
+
 		while (isNaN(numero)){
 			numero =prompt("ingrese nuevo numero");
 			numero = parseInt(numero);
@@ -19,14 +26,58 @@ function mostrar()
 	}
 
 		if(numero>0){
-			negativo = negativo + numero;
-		
-		}
+			acumuladorPositivo = acumuladorPositivos + numero;
+			contadorPositivos = contadorPositivos + 1;
 
-		respuesta = prompt("desea ingresar numero?");
+		} else{
+			if (numero < 0 ){
+				acumuladorNegativo= acumuladorNegativo + numero;
+				contadorNegativos = contadorNegativos + 1;
+			
+		} else{
+			contadorCeros = contadorCeros +1;
+		}
+	}  
+
+	if((numero/2) == 0 && numero != 0)
+	{
+		contadorPares ++;
+
+	}
+	
+	if (flag == false || numero > maximo)
+	{
+		maximo = numero;
+		flag == true;
+	}
+
+	respuesta =confirm("desea continuar?");
 	}
 
 
+	if(contadorPositivos != 0)
+	{
+		promedioPositivos = acumuladorPositivos/contadorPositivos;
+		document.write("promedio ")
+
+	} else
+	{
+		document.write("punto x. no se pudo calcular");
+	}
+
+	if(contadorNegativos !=0)
+	{
+		promedioNegativos = acumuladorNegativo/contadorNegativos;
+	}
+		
+	diferencia = acumuladorPositivos + acumuladorNegativo;
+
+		
+	
+
+
+document.write ("1.suma negativos" + acumuladorNegativo);
+document.write("2. suma positivos"+ acumuladorPositivos);
 
 
 }//FIN DE LA FUNCIÓN
